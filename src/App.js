c4443e20a862932ab5554612aa4ea9f9d117498e
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-29 05:49:01 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from 'react';
 import './App.scss';
 
 function App() {
+	const [name, setName] = useState('');
+	const [nameIsValid, setNameIsValid] = useState(false);
+	const [phone, setPhone] = useState('');
+	const [phoneIsValid, setPhoneIsValid] = useState(false);
+	const [payload, setPayload] = useState({});
+	const [formIsValid, setFormIsValid] = useState(false);
+
+	useEffect(() => {
+		setFormIsValid(nameIsValid && phoneIsValid);
+	}, [nameIsValid, phoneIsValid])
+
+	const clearPayload = () => {
+		setPayload(prev => ({}));
+	}
+
+	const handleName = (e) => {
+		let _name = e.target.value;
+		if (_name !== "" && _name.length <= 10) {
+			setNameIsValid(true);
+		} else {
+			setNameIsValid(false);
+		}
+		setName(_name);
+		clearPayload();
+	}
+
+	const handlePhone = (e) => {
+		let _phone = e.target.value;
+		if (_phone !== "" && /^\d\d\d-\d\d\d-\d\d\d\d$/.test(_phone)) {
+			setPhoneIsValid(true);
+		} else {
+			setPhoneIsValid(false);
+		}
+		setPhone(_phone);
+		clearPayload();
+	}
+
+	const handleButton = (e) => {
+		e.preventDefault();
+		setPayload(prev => ({
+			...prev,
+			name,
+			phone
+		}));
+	}
 	return (
 		<div className="App">
 			<form>
 				<fieldset>
 					<legend>Order Form</legend>
-					<div className="row">
+					<div className={"row " + (nameIsValid ? "valid" : "invalid")}>
 						<label htmlFor="name">Name</label>
-						<input type="text" id="name" />
+						<input type="text" id="name" value={name} onChange={handleName} />
 					</div>
-					<div className="row">
+					<div className={"note " + (nameIsValid ? "valid" : "invalid")}>required, maximum 10 characters</div>
+					<div className={"row " + (phoneIsValid ? "valid" : "invalid")}>
 						<label htmlFor="phone">Phone</label>
-						<input type="text" id="phone" />
+						<input type="text" id="phone" onChange={handlePhone} value={phone} />
 					</div>
+					<div className={"note " + (phoneIsValid ? "valid" : "invalid")}>e.g. 555-333-2222</div>
 					<div className="buttonRow">
-						<button>Register</button>
+						<button disabled={!formIsValid} onClick={handleButton}>Register</button>
 					</div>
 				</fieldset>
 			</form>
+			{Object.keys(payload).length !== 0 && (
+				<pre>
+					payload: {JSON.stringify(payload, null, 2)}
+				</pre>
+			)}
 		</div>
 	);
 }
